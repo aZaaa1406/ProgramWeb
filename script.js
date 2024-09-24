870e63d3ac90041f1b2c47b450371c9e123d07c3
aZaaa1406 <rvaldeza2200@alumno.ipn.mx>
2024-09-24 17:56:44 -0600

    Creacion de dir y archivo

---
+++ script.js
@@ -0,0 +1,24 @@
+//Clase 24/09/2024
+//Nombre: Valdez Avendaño Ricardo Azariel
+//variables
+
+/las variable sguardan informaion que puedes reutilizar/
+//declaracion de variables
+
+let nombre = 'pedro';//usa let para declarar variables que pueden cambira su valor
+const PI = 3.1416;
+var edad = 19;
+
+let a = 5;
+let b = 2;
+let suma = a + b;
+let resta = a - b;
+let multiplicacion = a * b;
+let division = a / b;
+
+//operadores de comparacion
+
+a > b;//true
+a < b;//false
+a == b;//false
+a === b;//false (compara valores y tipo)
