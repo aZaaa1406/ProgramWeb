622e195eef22fd03f504438639431a271232e89c
aZaaa1406 <rvaldeza2200@alumno.ipn.mx>
2024-10-22 17:43:38 -0600

    clase del dia

---
+++ clase22102024/index.js
@@ -0,0 +1,19 @@
+function hola(a,b){
+    return a +b
+}
+
+console.log(hola(3,5));
+
+const suma = (a,b)=>{
+    return a+b;
+}
+
+console.log(suma(3,5));
+
+//03 simplificar 
+const SUMA = (a,b)=> a+b
+console.log(SUMA(3,5));
+
+//04 solo un parametro
+const cuadrado = x=> x*x;
+console.log(cuadrado(2));
